886f500e3ab13532b249b53e53aac22220627f44
OlegPriemyshev <prolni@mail.ru>
2022-10-01 20:27:38 +0300

    СЕМИНАР 2 задача 1

--- seminar_2_task_1.py
+++ seminar_2_task_1.py
@@ -2,3 +2,13 @@
 # *Пример:*
 #  - 6782 -> 23
 #  - 0,56 -> 11
+
+str_num = input('Введите вещественное число: ')
+n = len(str_num)
+num = abs(int(float(str_num) * 10 ** n))
+digits_sum = 0
+while num >= 10:
+    digits_sum += num % 10
+    num //= 10
+digits_sum += num
+print(f"Сумма цифр в числе: {'{:.0f}'.format(digits_sum)}")
