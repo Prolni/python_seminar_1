b0538e87b44bc7bf675738f5c0ae35d8d56b20af
OlegPriemyshev <prolni@mail.ru>
2022-10-09 19:28:12 +0300

    СЕМИНАР 4 задача 5

--- seminar_4_task_5.py
+++ seminar_4_task_5.py
@@ -1,31 +1,20 @@
 # Даны два файла, в каждом из которых находится запись многочлена. 
-# Задача - сформировать файл, содержащий сумму многочленов. 2*x² + 4*x + 5 3*x² +10*x + 6 Вывод: 5*x² + 14*x + 11
+# Задача - сформировать файл, содержащий сумму многочленов. 2*x² + 4*x + 5  3*x² +10*x + 6 Вывод: 5*x² + 14*x + 11
 
-def lineCountInFile(path):
-    file = open(path, 'r')
-    lines = 0
-    for line in file:
-        if line != "\n":
-            lines += 1
-    return lines
+import sympy
 
+x = sympy.Symbol('x')
+with open("path1.txt", 'r') as path1:
+    a = path1.read()
+print(a)
 
-def mergeLinesInFles(path1, path2):
-    if lineCountInFile(path1) != lineCountInFile(path2):
-        print('The contents of the files do not match!')
-        return
-    poly1 = open(path1, 'r')
-    poly2 = open(path2, 'r')
-    with open('polySum.txt', 'w') as result:
-        for i in range(lineCountInFile(path1)):
-            result.write(poly1.readline().replace('=0\n', '+') +
-                         poly2.readline())
+with open("path2.txt", 'r') as path2:
+    b = path2.read()
+print(b)
 
-    poly1.close()
-    poly2.close()
+c = sympy.simplify(a+'+'+b)
 
+with open("sum=path1+path2.txt", 'w') as f:
+    f.write(str(c))
 
-path1 = 'poly1.txt'
-path2 = 'poly2.txt'
-
-mergeLinesInFles(path1, path2)
+print(c)
