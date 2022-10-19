74a22fc9f630bc58e798eb8f4c2eb47b17242cfd
OlegPriemyshev <prolni@mail.ru>
2022-10-19 11:00:46 +0300

    task_1

---
+++ task_1/123.py
@@ -0,0 +1,22 @@
+# a = 13
+# b = 12.98
+# print(b-a)
+# print(round(b-int(b),10))
+
+# res = [input()]
+# print(res)
+
+# lst = [1, 5, 2, 3, 4, 6, 1, 7]
+# lst2 = [5, 4, 3, 2, 1, 0, -1, -2]
+# lst3 = [1, 1, 1, 1, 1, 1, 1, 1]
+
+# def make_new_list(old_list):
+#     new_list = []
+#     for i in range(len(old_list)):
+#         if i == 0 or old_list[i] > new_list[-1]:
+#             new_list.append(old_list[i])
+#     return new_list
+
+# print(make_new_list(lst), make_new_list(lst2), make_new_list(lst3))
+
+
